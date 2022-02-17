c3ba2c7359ee8bc13f65b828a7b2749d41665fde
RicardoOliver <r.c.d.1985@hotmail.com>
2022-02-17 17:37:32 -0300

    Projeto Cypress com allure report

---
+++ cypress/plugins/index.js
@@ -0,0 +1,9 @@
+/// <reference types="cypress" />
+/// <reference types="@shelex/cypress-allure-plugin" />
+
+const allureWriter = require("@shelex/cypress-allure-plugin/writer")
+
+module.exports = (on, config) => {
+  allureWriter(on, config);
+  return config;
+}
